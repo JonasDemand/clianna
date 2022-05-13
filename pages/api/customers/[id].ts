bf193b7f3fdc5f84775571ea5edc3c7ff1141adc
Jonas Demand <jonas@demand-online.de>
2022-05-14 00:27:18 +0200

    Add revalidation on customer change

--- pages/api/customers/[id].ts
+++ pages/api/customers/[id].ts
@@ -48,6 +48,7 @@ const put = async (req: NextApiRequest, res: NextApiResponse) => {
     include: { orders: true },
     data: { ...body },
   });
+  await res.unstable_revalidate('/customers');
   if (customer == null) {
     res.status(404).send(null);
     return;
