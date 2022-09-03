b6eefdf7f679e2313a14ec540742a9c600ab71cd
Jonas Demand <jonas@demand-online.de>
2022-09-03 22:06:53 +0200

    Omit creationDate in order interaface

--- types/database/order.ts
+++ types/database/order.ts
@@ -2,7 +2,7 @@ import { Order } from '@prisma/client';
 
 import { ICustomer } from './customer';
 
-export interface IOrder extends Omit<Order, 'userId'> {}
+export interface IOrder extends Omit<Order, 'userId' | 'creationDate'> {}
 
 export interface IOrderWithCustomer extends IOrder {
   customer: ICustomer | null;
