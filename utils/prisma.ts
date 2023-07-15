dd432023369b7d1b221f1b322faf559cc06cf9f9
Jonas Demand <jonas@demand-online.de>
2023-07-15 14:33:16 +0200

    Add redis cache

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,5 +1,32 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { Redis } from 'ioredis';
+import { createPrismaRedisCache } from 'prisma-redis-middleware';
 
-let prisma = new PrismaClient();
+const redis = new Redis(); // Uses default options for Redis connection
+
+const prisma = new PrismaClient();
+
+const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+  storage: {
+    type: 'redis',
+    options: {
+      client: redis,
+      invalidation: { referencesTTL: 300 },
+      log: console,
+    },
+  },
+  cacheTime: 300,
+  onHit: (key) => {
+    console.log('hit', key);
+  },
+  onMiss: (key) => {
+    console.log('miss', key);
+  },
+  onError: (key) => {
+    console.log('error', key);
+  },
+});
+
+prisma.$use(cacheMiddleware);
 
 export default prisma;
