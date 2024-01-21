a99b99f2dbfbf53d8c6307e9a0888f3acc425289
Jonas Demand <jonas@demand-online.de>
2024-01-21 20:36:23 +0100

    debug logging

--- Server/Services/ExternalApis/GoogleService.cs
+++ Server/Services/ExternalApis/GoogleService.cs
@@ -4,20 +4,21 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Docs.v1;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Services.ExternalApis;
 
 public class GoogleService : IGoogleService
 {
-    public GoogleService(IOptions<AppSettings> appSettings)
+    public GoogleService(IOptions<AppSettings> appSettings, ILogger<GoogleService> logger)
     {
         Stream stream;
         if (string.IsNullOrEmpty(appSettings.Value.GoogleOptions.DEV_ServiceAccountJson))
         {
             stream = new MemoryStream();
             JsonSerializer.Serialize(stream, appSettings.Value.GoogleOptions.ServiceAccountCredentials);
-
+            logger.LogInformation(JsonSerializer.Serialize(appSettings.Value.GoogleOptions.ServiceAccountCredentials)); //TODO: remove
             stream.Position = 0; //Somehow this is needed to read the stream afterwards
         }
         else
